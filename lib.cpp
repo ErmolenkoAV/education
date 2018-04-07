9b33c69bd5e00d3744a73a583944ca605aa2c630
Alexey Ermolenko <ErmolenkoAV@yandex.ru>
2018-04-07 22:06:14 +0300

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,8 +3,8 @@
 #include "lib.h"
 
 std::string fullVersion() {
-  auto version = std::to_string(PROJECT_VERSION_MAJOR) " + " 
-                        std::to_string(PROJECT_VERSION_MINOR) " + " 
+  auto version = std::to_string(PROJECT_VERSION_MAJOR) + "." + 
+                        std::to_string(PROJECT_VERSION_MINOR) + "." +
 		        std::to_string(PROJECT_VERSION_PATCH);
   return version;
 }
