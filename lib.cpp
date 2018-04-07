9f37f0ce3b0c4eac20ba34795b88ec32277654f7
Alexey Ermolenko <ErmolenkoAV@yandex.ru>
2018-04-07 21:47:15 +0300

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,9 +2,13 @@
 #include <string>
 #include "lib.h"
 
-std::string version() {
-	std::string version = std::to_string(PROJECT_VERSION_MAJOR) " + " 
-                              std::to_string(PROJECT_VERSION_MINOR) " + " 
-		              std::to_string(PROJECT_VERSION_PATCH);
-	return version;
+std::string fullVersion() {
+  std::string version = std::to_string(PROJECT_VERSION_MAJOR) " + " 
+                        std::to_string(PROJECT_VERSION_MINOR) " + " 
+		        std::to_string(PROJECT_VERSION_PATCH);
+  return version;
+}
+
+int version() {
+  return PROJECT_VERSION_PATCH;
 }
